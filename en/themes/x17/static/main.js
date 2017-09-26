10707b6eeff34b11f0b065324d56d0b119647ba8
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2017-09-25 20:03:12 -0700

    Move sidenotes

--- en/themes/x17/static/main.js
+++ en/themes/x17/static/main.js
@@ -1,1 +1,40 @@
 require('prismjs')
+
+function moveFootnotesToRight() {
+  var xs = document.querySelectorAll("article a[rel='footnote']")
+  xs.forEach(function (a) {
+    var href = a.hash.split(/:/)
+    var id = 'fn:' + href[1];
+
+    var li = document.getElementById(id);
+    li.style.position = 'absolute';
+    li.style.top = a.offsetTop + 'px';
+  });
+}
+
+function moveFootnotesToBottom() {
+  var xs = document.querySelectorAll("article a[rel='footnote']")
+  xs.forEach(function (a) {
+    var href = a.hash.split(/:/)
+    var id = 'fn:' + href[1];
+
+    var li = document.getElementById(id);
+    li.style.position = 'static';
+  });
+}
+
+function moveFootnotes() {
+  if (document.body.offsetWidth < 800) {
+    moveFootnotesToBottom();
+  } else {
+    moveFootnotesToRight();
+  }
+}
+
+window.addEventListener('resize', function () {
+  moveFootnotes();
+});
+
+setTimeout(function () {
+  moveFootnotes();
+}, 1);
